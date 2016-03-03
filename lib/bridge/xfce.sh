222bbec9d818b5fa6778a281f9ae36a18ec34f63
ashysgit <ashysmail@gmail.com>
2016-03-03 10:15:51 +0100

    xfce wallpaper script

---
+++ lib/bridge/xfce.sh
@@ -0,0 +1,13 @@
+#http://askubuntu.com/questions/414422/command-to-change-the-wallpaper-in-xubuntu
+
+connectedOutputs=$(xrandr | grep " connected" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/")
+activeOutput=$(xrandr | grep -e " connected [^(]" | sed -e "s/\([A-Z0-9]\+\) connected.*/\1/") 
+connected=$(echo $connectedOutputs | wc -w)
+
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -n -t string -s  $1
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorLVDS1/workspace0/last-image -n -t string -s  $1
+
+for i in $(xfconf-query -c xfce4-desktop -p /backdrop -l|egrep -e "screen.*/monitor.*image-path$" -e "screen.*/monitor.*/last-image$"); do
+    xfconf-query -c xfce4-desktop -p $i -n -t string -s $1 
+    xfconf-query -c xfce4-desktop -p $i -s $1
+done
